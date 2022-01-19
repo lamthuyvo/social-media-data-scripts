2f6a8ceb92051caf58f382243535af1011739dc6
Deepak Mittal <50262096+yesdeepakmittal@users.noreply.github.com>
2022-01-19 13:02:14 +0530

    Create secrets.py

---
+++ 01-apis/scripts/secrets.py
@@ -0,0 +1,15 @@
+# Twitter API credentials, you can get them here https://apps.twitter.com/
+TWITTER_C_KEY = ""
+TWITTER_C_SECRET = ""
+TWITTER_A_KEY = ""
+TWITTER_A_SECRET = ""
+
+# Facebook API credentials
+FACEBOOK_APP_ID = ""
+FACEBOOK_APP_SECRET = ""
+
+# YouTube credentials
+YOUTUBE_API_KEY = ""
+
+# Giphy credentials
+GIPHY_API_KEY = ""
