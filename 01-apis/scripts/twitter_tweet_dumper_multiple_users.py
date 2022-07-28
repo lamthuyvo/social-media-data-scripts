00940886e6cea320ff6221d01d681cc52ccfa600
lamthuyvo <lamthuyvo@gmail.com>
2022-07-27 23:49:26 -0400

    modified script that pulls data from multiple accounts to write csvs into the output folder

--- 01-apis/scripts/twitter_tweet_dumper_multiple_users.py
+++ 01-apis/scripts/twitter_tweet_dumper_multiple_users.py
@@ -89,7 +89,7 @@ def get_all_tweets(screen_name):
 
 
 	#write the csv
-	with open_csv_w('%s_tweets.csv' % screen_name) as f:
+	with open_csv_w('../output/%s_tweets.csv' % screen_name) as f:
 		writer = csv.writer(f)
 		writer.writerow(["id",
 				"created_at",
